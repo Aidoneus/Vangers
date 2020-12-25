1a764d4e5884f4fb7de2206362daef434479fe3c
Alexander Dormidontov <aid@kiv.name>
2020-12-25 11:00:13 +0400

    server.cpp breaking net compatibility fix

--- server/server.cpp
+++ server/server.cpp
@@ -2180,7 +2180,7 @@ void Server::analyse_statistics(Game *g) {
 
 void Server::get_games_list(OutputEventBuffer &out_buffer, int client_version) {
 	int num = 0;
-	char gameTypeLetter = "-";
+	char* gameTypeLetter = "-";
 	Game *g = games.first();
 	while (g) {
 		if (g->data.GameType != UNCONFIGURED && g->used_players_IDs != 0x7fffffff &&
